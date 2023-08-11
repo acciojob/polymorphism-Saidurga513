bbbd300dfc3f123461bc3cd189cca8316da82591
Saidurga513 <saidurgak1998@gmail.com>
2023-08-11 19:22:01 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,32 @@
 package com.driver;
 
 public class Main {
+    public static class Product
+    {
+        public int product(int x, int y)
+        {
+            int c=x*y;
+            return c;
+        }
+        public int product(int x, int y,int z)
+        {
+            int c=x+y+z;
+            return c;
+        }
+        public  double product(double x,double y)
+        {
+            double c=x+y;
+            return c;
+        }
+    }
+    public static void main(String args[])
+    {
+        Product p=new Product();
+        System.out.println(
+        p.product(10,2));
+       System.out.println( p.product(10,5,3));
+        System.out.println(p.product(123456.0,12345.0));
 
+
+    }
 }
