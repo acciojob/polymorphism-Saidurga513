32a23baef0c92af70f018b8001e9379682d2aa51
Saidurga513 <saidurgak1998@gmail.com>
2023-08-12 16:50:10 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -21,10 +21,9 @@ public class Main
     public static void main(String args[])
     {
         Product p=new Product();
-        System.out.println(
-        p.product(10,2));
-       System.out.println( p.product(10,5,3));
-        System.out.println(p.product(123456.0,12345.0));
+        p.product(10,2);
+      p.product(10,5,3);
+       p.product(123456.0,12345.0);
 
 
     }
